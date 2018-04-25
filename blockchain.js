860098b2331037b3f96bac8058a3e76d2a7e2dfb
James McDonagh <james.mcdonagh@statera.com>
2018-04-25 17:00:26 -0600

    Chain Validity
    Added function that checks the validity of upcoming chains

--- blockchain.js
+++ blockchain.js
@@ -11,6 +11,24 @@ class Blockchain {
 
         return block;
     }
+    //in order to support multiple contributers to our blockchain, we need to add a funtion that checks the validity of upcoming chains
+    isValidChain(chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+    //if the chain is invalid the function will return false otherwise the chain is ok and the function returns true 
+    //Note that two different objects that aren't referencing the same original object cannot be equal to each other even if they have the same exact elements
+        
+        for (let i = 1; i < chain.length; i++) {
+            const block = chain[i];
+            const lastBlock = chain[i - 1];
+
+            if (block.lastHash !== lastBlock.hash ||
+                block.hash !== Block.blockHash(block)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 module.exports = Blockchain;
