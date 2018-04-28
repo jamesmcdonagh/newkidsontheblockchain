f1ee98fcf9d740931a45e55b8930d5f7799aeed1
jamesmcdonagh <james.mcdonagh@statera.com>
2018-04-28 12:49:43 -0600

    Dynamic Block Difficulty
    added a way to adjust the difficulty based on the lastblock.timestamp + mine_rate > currenttime, which will adjust the diffculty +/- 1

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,53 +1,65 @@
 const SHA256 = require('crypto-js/sha256');
-const { DIFFICULTY } = require('../config');
+const { DIFFICULTY, MINE_RATE } = require('../config');
 
 class Block {
-    constructor(timestamp, lastHash, hash, data, nonce) {
+    constructor(timestamp, lastHash, hash, data, nonce, difficulty) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
         this.nonce = nonce;//value that is included as the calculation of the hash for the block
+        this.difficulty = difficulty || DIFFICULTY;
     }
 
     toString() {
         return `Block -
-            Timestamp: ${this.timestamp}
-            Last Hash: ${this.lastHash.substring(0, 10)}
-            Hash     : ${this.hash.substring(0, 10)}
-            Nonce    : ${this.nonce}
-            Data     : ${this.data}`;
+            Timestamp : ${this.timestamp}
+            Last Hash : ${this.lastHash.substring(0, 10)}
+            Hash      : ${this.hash.substring(0, 10)}
+            Nonce     : ${this.nonce}
+            Difficulty: ${this.difficulty}
+            Data      : ${this.data}`;
     }
 
     static genesis() {
-        return new this('Genesis time', '------', 'f1r57-h45h', [], 0);
+        return new this('Genesis time', '------', 'f1r57-h45h', [], 0, DIFFICULTY);
     }
 
     static mineBlock(lastBlock, data) {
         let hash, timestamp;
         const lastHash = lastBlock.hash;
+        let { difficulty } = lastBlock;
         let nonce = 0;
 //this is proof-of-work algorithim in action. it takes a substring of the generated hash and
 //it makes sure that it maches a certain number of leading zeroes up to the blockchains difficulty
         do {
             nonce++;
             timestamp = Date.now();
-            hash = Block.hash(timestamp, lastHash, data, nonce);
-        } while (hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY));
+            //lastblock gives access to timestamp and the difficulty of the blcok that came before it
+            //and timestamp helps understand how to adjust that difficulty
+            difficulty = Block.adjustDifficulty(lastBlock, timestamp);
+            hash = Block.hash(timestamp, lastHash, data, nonce, difficulty);
+        } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
 //this return ensures blocks are generated with the proper hash value to match our leading zero condition
 //by adding a loop to generate these hash values we're demanding that the node requesting to
 //mine a block is actually spending computational power to find this hash value
-        return new this(timestamp, lastHash, hash, data, nonce);
+        return new this(timestamp, lastHash, hash, data, nonce, difficulty);
     }
-
+//START HASH FUNCTIONS
     static hash(timestamp, lastHash, data, nonce) {
-        return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString();
+        return SHA256(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString();
     }
 
     static blockHash(block) {
-        const { timestamp, lastHash, data, nonce } = block;
-        return Block.hash(timestamp, lastHash, data, nonce);
+        const { timestamp, lastHash, data, nonce, difficulty } = block;
+        return Block.hash(timestamp, lastHash, data, nonce, difficulty);
     }
-}
+
+    static adjustDifficulty(lastBlock, currentTime) {
+        let { difficulty } = lastBlock;
+        difficulty = lastBlock.timestamp + MINE_RATE > currentTime ? difficulty + 1 : difficulty - 1
+        return difficulty;
+    }
+}//STOP HASH FUNTIONS
 
 module.exports = Block;
